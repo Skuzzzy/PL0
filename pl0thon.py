1906b7b870f7b81d177d14cad06c2d7f55d239b9
Daniel Baird <JamesDanielBaird@gmail.com>
2016-06-25 23:18:56 -0700

    Get to AST creation

--- pl0thon.py
+++ pl0thon.py
@@ -1,5 +1,6 @@
 
 import fileinput
+import time
 
 
 
@@ -13,7 +14,7 @@ class Reader(object):
     def ignore_whitespace(self):
         while self.index < len(self.code) and self.peek().isspace():
             self.forward()
-            print self.index
+            # print self.index
 
     def peek(self):
         return self.code[self.index]
@@ -80,63 +81,63 @@ class Tokenizer(object):
         while self.reader.has_next():
             self.reader.ignore_whitespace()
             peeked = self.reader.peek()
-            if "=" == peeked:
+            if '=' == peeked:
                 self.tokens.append(Token(TokenType.EQUALS, peeked))
                 self.reader.forward()
-            elif "," == peeked:
+            elif ',' == peeked:
                 self.tokens.append(Token(TokenType.COMMA, peeked))
                 self.reader.forward()
-            elif "?" == peeked:
+            elif '?' == peeked:
                 # Literally no idea what this is
                 self.tokens.append(Token(TokenType.QMARK, peeked))
                 self.reader.forward()
-            elif "!" == peeked:
+            elif '!' == peeked:
                 self.tokens.append(Token(TokenType.BANG, peeked))
                 self.reader.forward()
-            elif ";" == peeked:
+            elif ';' == peeked:
                 self.tokens.append(Token(TokenType.SEMICOLON, peeked))
                 self.reader.forward()
-            elif "+" == peeked:
+            elif '+' == peeked:
                 self.tokens.append(Token(TokenType.PLUS, peeked))
                 self.reader.forward()
-            elif "-" == peeked:
+            elif '-' == peeked:
                 self.tokens.append(Token(TokenType.MINUS, peeked))
                 self.reader.forward()
-            elif "*" == peeked:
+            elif '*' == peeked:
                 self.tokens.append(Token(TokenType.MULT, peeked))
                 self.reader.forward()
-            elif "/" == peeked:
+            elif '/' == peeked:
                 self.tokens.append(Token(TokenType.DIV, peeked))
                 self.reader.forward()
-            elif "{" == peeked:
+            elif '{' == peeked:
                 self.tokens.append(Token(TokenType.PAREN_OPEN, peeked))
                 self.reader.forward()
-            elif "}" == peeked:
+            elif '}' == peeked:
                 self.tokens.append(Token(TokenType.PAREN_CLOSE, peeked))
                 self.reader.forward()
-            elif "." == peeked:
+            elif '.' == peeked:
                 self.tokens.append(Token(TokenType.FULL_STOP, peeked))
                 self.reader.forward()
-            elif ":" == peeked:
+            elif ':' == peeked:
                 self.reader.forward()
-                if not self.reader.peek() == "=":
+                if not self.reader.peek() == '=':
                     raise Exception()
-                self.tokens.append(Token(TokenType.ASSIGNMENT, ":="))
+                self.tokens.append(Token(TokenType.ASSIGNMENT, ':='))
                 self.reader.forward()
-            elif "<" == peeked:
+            elif '<' == peeked:
                 self.reader.forward()
-                if not self.reader.peek() == "=":
-                    self.tokens.append(Token(TokenType.LT_EQ, "<="))
+                if self.reader.peek() == '=':
+                    self.tokens.append(Token(TokenType.LT_EQ, '<='))
                     self.reader.forward()
                 else:
-                    self.tokens.append(Token(TokenType.LT, "<"))
-            elif ">" == peeked:
+                    self.tokens.append(Token(TokenType.LT, '<'))
+            elif '>' == peeked:
                 self.reader.forward()
-                if not self.reader.peek() == "=":
-                    self.tokens.append(Token(TokenType.GT_EQ, "<="))
+                if self.reader.peek() == '=':
+                    self.tokens.append(Token(TokenType.GT_EQ, '<='))
                     self.reader.forward()
                 else:
-                    self.tokens.append(Token(TokenType.GT, "<"))
+                    self.tokens.append(Token(TokenType.GT, '<'))
             elif peeked.isdigit():
                 self.tokens.append(Token(TokenType.NUMBER, self.parse_number()))
             elif peeked.isalpha():
@@ -146,9 +147,11 @@ class Tokenizer(object):
                 else:
                     self.tokens.append(Token(TokenType.IDENTIFIER, identifier))
             else:
-                print "UNHANDLED CASE", peeked
+                print 'UNHANDLED CASE', ord(peeked)
+                self.reader.forward()
+                time.sleep(1)
 
-            print self.tokens
+            # print self.tokens
 
     def parse_identifier(self):
         start_index = self.reader.index
@@ -169,31 +172,202 @@ class Tokenizer(object):
         return self.reader.get_range(start_index, end_index)
 
 
-# class Parser(object):
-    # def __init__(self, tokens):
-        # self.tokens = tokens
-        # self.index = 0
-
-    # def produce_tokens(self):
-        # self.program()
-        # self.expect('.')
-
-    # def expect(self, expected):
-        # if not self.tokens[self.index][1] == expected:
-            # raise Exception()
-
-    # def optional(self, value)
-        # return self.tokens[self.index][1] == expected:
-
-    # def program(self):
-        # self.block()
-        # self.expect('.')
-
-    # def block(self):
-        # if self.optional('const'):
-            # pass
+class Parser(object):
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.index = 0
 
 
-program = "".join([line for line in fileinput.input()])
+        print "\n".join([str(x) for x in self.tokens])
+        print self.program()
+
+    def debug(self):
+        print self.index
+        print self.tokens[self.index]
+
+    def produce_tokens(self):
+        self.program()
+        self.expect('.')
+
+    def expect(self, expected):
+        token = self.tokens[self.index]
+        if not token.literal.lower() == expected:
+            raise Exception()
+        self.index += 1
+        return token
+
+    def expect_type(self, expected_type):
+        token = self.tokens[self.index]
+        if not token.token_type == expected_type:
+            raise Exception()
+        self.index += 1
+        return token
+
+    def optional(self, value, consume=True):
+        option = self.tokens[self.index].literal.lower() == value
+        if option and consume:
+            self.index += 1
+        return option
+
+    def optional_type(self, value, consume=True):
+        option = self.tokens[self.index].token_type == value
+        if option and consume:
+            self.index += 1
+        return option
+
+    def program(self):
+        blok = self.block()
+        self.expect('.')
+        return blok
+
+    def block(self):
+        consts = []
+        if self.optional('const'):
+            ident = self.expect_type(TokenType.IDENTIFIER)
+            self.expect('=')
+            number = self.expect_type(TokenType.NUMBER)
+            consts.append((ident, number))
+            while self.optional(','):
+                ident = self.expect_type(TokenType.IDENTIFIER)
+                self.expect('=')
+                number = self.expect_type(TokenType.NUMBER)
+                consts.append((ident, number))
+            self.expect(';')
+        variables = []
+        if self.optional('var'):
+            ident = self.expect_type(TokenType.IDENTIFIER)
+            variables.append(ident)
+            while self.optional(','):
+                ident = self.expect_type(TokenType.IDENTIFIER)
+                variables.append(ident)
+            self.expect(';')
+
+        procedures = []
+        while self.optional('procedure'):
+            ident = self.expect_type(TokenType.IDENTIFIER)
+            self.expect(';')
+            subblock = self.block()
+            self.expect(';')
+            procedures.append((ident, subblock))
+        stmnt = self.statement()
+        return (consts, variables, procedures, stmnt)
+
+    def statement(self):
+        if self.optional_type(TokenType.IDENTIFIER, consume=False):
+            ident = self.expect_type(TokenType.IDENTIFIER)
+            self.debug()
+            assign = self.expect_type(TokenType.ASSIGNMENT)
+            expr = self.expression()
+            return (assign, ident, expr)
+        elif self.optional('call', consume=False):
+            call = self.expect('call')
+            ident = self.expect_type(TokenType.IDENTIFIER)
+            return (call, ident)
+        elif self.optional('?'):
+            ident = self.expect_type(TokenType.IDENTIFIER)
+            return ('?', ident)
+        elif self.optional('!'):
+            expr = self.expression()
+            return ('!', expr)
+        elif self.optional('begin'):
+            statements = []
+            stmnt = self.statement()
+            statements.append(stmnt)
+            while self.optional(';'):
+                stmnt = self.statement()
+                statements.append(stmnt)
+            self.expect('end')
+            return ('statements', statements)
+        elif self.optional('if'):
+            cond = self.condition()
+            self.expect('then')
+            stmnt = self.statement()
+            return ('if', cond, stmnt)
+        elif self.optional('while'):
+            cond = self.condition()
+            self.expect('do')
+            stmnt = self.statement()
+            return ('while', cond, stmnt)
+        else:
+            self.debug()
+            raise Exception()
+
+    def condition(self):
+        if self.optional('odd'):
+            expr = self.expression()
+            return ('odd', expr)
+        else:
+            first_expr = self.expression()
+
+            # FIXME Lazy Hack
+            cmp_op = self.tokens[self.index]
+            self.index += 1
+
+            second_expr = self.expression()
+
+            return (first_expr, cmp_op, second_expr)
+
+    def is_plus_minus(self):
+        return self.tokens[self.index].literal in set(['+', '-'])
+
+    def is_minus(self):
+        boolean = self.tokens[self.index].literal == '+'
+        self.index += 1
+        return boolean
+
+    def expression(self):
+        termpairs = []
+        sign = '+'
+        if self.is_plus_minus():
+            if self.is_minus():
+                sign = '-'
+        term = self.term()
+        termpairs.append((sign, term))
+        while self.is_plus_minus():
+            sign = '+' # Everything is implicit positive num
+            # FIXME Implicitely assumes addition
+            if self.is_minus():
+                sign = '-'
+            term = self.term()
+            termpairs.append((sign, term))
+        return termpairs
+
+    def is_mult_div(self):
+        return self.tokens[self.index].literal in set(['*', '/'])
+
+    def is_div(self):
+        boolean = self.tokens[self.index].literal == '/'
+        self.index += 1
+        return boolean
+
+    def term(self):
+        factorlist = []
+        factor = self.factor()
+        factorlist.append(factor)
+        while self.is_mult_div():
+            operation = "*"
+            # FIXME Implicitely assumes multiplication
+            if self.is_div():
+                operation = "/"
+
+            factor = self.factor()
+            factorlist.append((operation, factor))
+        return factorlist
+
+    def factor(self):
+        if self.optional_type(TokenType.IDENTIFIER, consume=False):
+            factor = self.expect_type(TokenType.IDENTIFIER)
+            return factor
+        elif self.optional_type(TokenType.NUMBER, consume=False):
+            factor = self.expect_type(TokenType.NUMBER)
+            return factor
+        else:
+            self.debug()
+            self.expect('(')
+            expr = self.expression()
+            self.expect(')')
+            return expr
+
+program = ''.join([line for line in fileinput.input()])
 tokenizer = Tokenizer(program)
 parser = Parser(tokenizer.get_tokens())
